2ee63137ee974a2871edb175c1f8f57d2254d7c6
Anton <kalistratov.ant@gmail.com>
2021-03-29 20:39:02 +0500

    git init

---
+++ ShoppingListTelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingListTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
